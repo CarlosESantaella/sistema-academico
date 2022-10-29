dd97a81ee29e57f7b3d4f67ff1f667fa20f7e4a8
Hildegar Medina <medinahildegar1@gmail.com>
2022-10-28 21:31:03 -0300

    Migrate

--- database/migrations/2022_10_29_003043_add_foreign_keys_to_dicta_table.php
+++ database/migrations/2022_10_29_003043_add_foreign_keys_to_dicta_table.php
@@ -14,9 +14,9 @@ class AddForeignKeysToDictaTable extends Migration
     public function up()
     {
         Schema::table('dicta', function (Blueprint $table) {
+            $table->foreign(['idmateria'], 'dicta_ibfk_2')->references(['codigo'])->on('materia');
             $table->foreign(['idusuario'], 'dicta_ibfk_1')->references(['codigo'])->on('usuario');
             $table->foreign(['idcurso'], 'dicta_ibfk_3')->references(['codigo'])->on('curso');
-            $table->foreign(['idmateria'], 'dicta_ibfk_2')->references(['codigo'])->on('materia');
         });
     }
 
@@ -28,9 +28,9 @@ class AddForeignKeysToDictaTable extends Migration
     public function down()
     {
         Schema::table('dicta', function (Blueprint $table) {
+            $table->dropForeign('dicta_ibfk_2');
             $table->dropForeign('dicta_ibfk_1');
             $table->dropForeign('dicta_ibfk_3');
-            $table->dropForeign('dicta_ibfk_2');
         });
     }
 }
