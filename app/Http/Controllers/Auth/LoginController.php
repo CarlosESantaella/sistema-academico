e1bf418861a26f01880ddd243056b2c941c885a5
Carlos Santaella <carlos.santaella.cs@gmail.com>
2022-10-29 08:45:52 -0500

    añadido archivo 'informacion_importante.txt' en la raiz del proyecto para incluir los puntos importantes del proyecto

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -19,6 +19,31 @@ class LoginController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'user' => 'required',
+            'password' => 'required',
+            'user_type' => 'required'
+        ]);
 
+        $user = $request->user;
+        $password = $request->password;
+        $user_type = $request->user_type;
+
+        if($user_type == 'estudiante'){
+
+        }
+        // else if(){
+
+        // }else if(){
+
+        // }else if(){
+
+        // }
+        
+        if(!auth()->attempt($request->only('email', 'password'))){
+            return back()->with('error_login', 'Usuario o contraseña incorrectos');
+        }
+
+        return redirect()->
     }
 }
