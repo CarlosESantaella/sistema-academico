11c5f79d9c352a0eed1f3dc527b03586077ef030
Hildegar Medina <medinahildegar1@gmail.com>
2022-11-01 20:21:51 -0300

    Change status y modal

--- resources/views/students/edit-student.blade.php
+++ resources/views/students/edit-student.blade.php
@@ -1,11 +1,42 @@
 @extends('layouts.layout')
 @section('title', 'Home')
 @section('content')
+<script>
+    function changeStatus(id_estudiante, estado) {
+        fetch(
+            "/students/"+id_estudiante+"/changeState",
+            {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    'estado': estado
+                }) 
+            }
+        )
+    }
+    let id_estudiante = "{{$student->codigo}}";
+    let estado = "{{$student->estado}}";
+    if (estado == "-1") {
+        if (confirm("¿Desea matricularse en el siguiente periodo escolar?")) {
+            changeStatus(id_estudiante, "1");
+        }else {
+            changeStatus(id_estudiante, "-1");
+        }
+    }
+</script>
 <main>
-    <div class="container-fluid">
-        <form method="POST" action="/students/{{$student->codigo}}">
-            <x-students.edit.edit-student :student="$student" :responsibles="$responsibles"/>
-        </form>
-    </div>
+    @if ($student->estado != '1')
+        <div class="container-fluid">
+            <p>Esta sección esta bloqueada</p>
+        </div>
+    @else
+        <div class="container-fluid">
+            <form method="POST" action="/students/{{$student->codigo}}">
+                <x-students.edit.edit-student :student="$student" :responsibles="$responsibles"/>
+            </form>
+        </div>
+    @endif
 </main>
 @endsection
