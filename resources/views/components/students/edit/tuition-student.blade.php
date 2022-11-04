7d80bd566c6b66c42c59203ec9b74211b439a56e
Hildegar Medina <medinahildegar1@gmail.com>
2022-11-04 09:08:07 -0300

    Agregar campos faltantes a editar usuario

--- resources/views/components/students/edit/tuition-student.blade.php
+++ resources/views/components/students/edit/tuition-student.blade.php
@@ -1,9 +1,9 @@
 <div class="row">
-    <div class="card col-12 col-xl-8 px-0 my-3 my-md-0">
-        <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+    <div class="card col-12 col-lg-8 col-xl-9 px-0 my-3 my-md-0">
+        <ul class="nav nav-pills mb-0 border-bottom bg-light" id="pills-tab" role="tablist">
             <li class="nav-item" role="presentation">
                 <button 
-                    class="nav-link active" 
+                    class="nav-link border-start rounded-0 active" 
                     id="unidad-educativa-tab-pane-tab" 
                     data-bs-toggle="pill" 
                     data-bs-target="#unidad-educativa-tab-pane" 
@@ -15,7 +15,7 @@
             </li>
             <li class="nav-item" role="presentation">
                 <button 
-                    class="nav-link"
+                    class="nav-link border-start rounded-0"
                     id="historial-tab-pane-tab"
                     data-bs-toggle="pill"
                     data-bs-target="#historial-tab-pane"
@@ -27,7 +27,7 @@
             </li>
             <li class="nav-item" role="presentation">
                 <button 
-                    class="nav-link"
+                    class="nav-link border-start rounded-0"
                     id="facturacion-tab-pane-tab"
                     data-bs-toggle="pill"
                     data-bs-target="#facturacion-tab-pane"
@@ -47,12 +47,37 @@
                 </div>
             </div>
             <div class="tab-pane fade p-4" id="historial-tab-pane" role="tabpanel" aria-labelledby="historial-tab"
-                tabindex="0">...</div>
+                tabindex="0">
+                <p class="mb-0">
+                    Código: 541357 | Fecha de inscripción: La Paz, 10 de enero de 2022 | Gestión 2022
+                </p>
+                <p class="mb-0">
+                    Código: 543214 | Fecha de inscripción: La Paz, 7 de enero de 2021 | Gestión 2021
+                </p>
+                <p class="mb-0">
+                    Código: 213215 | Fecha de inscripción: La Paz, 23 de enero de 2020 | Gestión 2020
+                </p>
+                <p class="mb-0">
+                    Código: 873645 | Fecha de inscripción: La Paz, 14 de enero de 2019 | Gestión 2019
+                </p>
+                <p class="mb-0">
+                    Código: 549876 | Fecha de inscripción: La Paz, 09 de enero de 2018 | Gestión 2018
+                </p>
+            </div>
             <div class="tab-pane fade p-4" id="facturacion-tab-pane" role="tabpanel" aria-labelledby="facturacion-tab"
-                tabindex="0">...</div>
+                tabindex="0">
+                <div class="input-group mb-3">
+                    <span class="input-group-text">Nombre: </span>
+                    <input type="text" name="fnombre" value="{{$student->fnombre}}" class="form-control">
+                </div>
+                <div class="input-group">
+                    <span class="input-group-text">NIT: </span>
+                    <input type="text" name="nit" value="{{$student->nit}}" class="form-control">
+                </div>
+            </div>
         </div>
     </div>
-    <div class="col-12 col-xl-4 my-3 my-xl-0">
+    <div class="col-12 col-lg-4 col-xl-3 my-3 my-xl-0">
         <button type="button" class="btn btn-primary-custom btn-lg">Nueva Matricula</button>
     </div>
 </div>
