8e2a048d1b4690be7c34953ae33464cf7b31efde
Carlos Santaella <carlos.santaella.cs@gmail.com>
2022-11-28 11:21:51 -0500

    listo

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '546b4ec01a5cc17ff865babac589a5191efae019',
+        'reference' => '1c6e461365768f1ed4936732cd51e6898310fb5e',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -421,7 +421,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '546b4ec01a5cc17ff865babac589a5191efae019',
+            'reference' => '1c6e461365768f1ed4936732cd51e6898310fb5e',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
